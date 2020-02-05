cea5cef82ba38590343c16a59dbc2cbd1828c7e7
Solomar <philippe.rosa-pong@outlook.com>
2020-02-06 01:13:57 +0900

    Update to the title screen. Cleaned up the root folder a bit.

---
+++ Assets/Scripts/FadingBitmapShader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class FadingBitmapShader : MonoBehaviour
+{
+    public float _currentValue;
+    public float _minFadeValue;
+    public float _maxFadeValue;
+    public float _fadingSpeed;
+
+    private TMP_Text _textMeshProElement;
+    private int _propertyID;
+    private bool _fading;
+
+    void Start()
+    {
+        _textMeshProElement = GetComponent<TMP_Text>();
+        //_propertyID = Shader.PropertyToID("_FadeValue");
+        //_textMeshProElement.material.SetFloat(_propertyID, _currentValue);
+    }
+
+    void LateUpdate()
+    {
+        if(_fading)
+            _currentValue -= Time.deltaTime / _fadingSpeed;
+        else
+            _currentValue += Time.deltaTime / _fadingSpeed;
+        _currentValue = Mathf.Clamp(_currentValue, _minFadeValue, _maxFadeValue);
+        if (_currentValue == _maxFadeValue || _currentValue == _minFadeValue) _fading = !_fading;
+        //_textMeshProElement.material.SetFloat(_propertyID, _currentValue);
+        _textMeshProElement.color = new Color(1f,1f,1f,_currentValue);
+    }
+}
---
+++ Assets/Scripts/LevelElements/TitleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    private bool gameStartCalled;
+    [SerializeField]
+    private Animator fadeControl;
+    [SerializeField]
+    private AudioClip startGameSound;
+
+    private void Start()
+    {
+        gameStartCalled = false;
+    }
+
+    void Update()
+    {
+        if (Input.anyKey && !gameStartCalled)
+        {
+            gameStartCalled = true;
+            fadeControl.SetTrigger("GameStart");
+            SoundManager.Instance.PlaySound(startGameSound);
+        }
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
